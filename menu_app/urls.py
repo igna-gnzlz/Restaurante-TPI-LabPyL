efc29f6f57bab9f69be88be8d87f05a10211b4f5
IgnacioGonzalez <gonzalezignacio@gmail.com>
2025-06-30 16:19:20 -0300

    Carrito funcional, creación de pedidos correcta, falta mostrarlos en historial de pedidos

--- menu_app/urls.py
+++ menu_app/urls.py
@@ -3,10 +3,6 @@ from .views import (
     MenuListView,
     ProductDetailView,
     AddToOrderView,
-    AddOneView,
-    RemoveOneView,
-    DeleteItemView,
-    CancelOrderView,
 )
 
 app_name = 'menu_app'
@@ -16,8 +12,4 @@ urlpatterns = [
     path("<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path('add-to-order/<int:pk>/', AddToOrderView.as_view(), name='add_to_order'),
     path("accounts/", include("accounts_app.urls", namespace="accounts_app")),
-    path('add_one/', AddOneView.as_view(), name='add_one'),
-    path('remove_one/', RemoveOneView.as_view(), name='remove_one'),
-    path('delete_item/', DeleteItemView.as_view(), name='delete_item'),
-    path('cancel_order/', CancelOrderView.as_view(), name='cancel_order')
 ]
--- restaurante/urls.py
+++ restaurante/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
-from menu_app.views import HomeView, MyOrdersView
+from menu_app.views import HomeView, MakeOrderView, ConfirmOrderView, DecrementFromCartView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("menu/", include(("menu_app.urls", "menu_app"), namespace="menu_app")),
+    path('make_order/', MakeOrderView.as_view(), name='make_order'),
+    path('decrement-from-cart/<int:pk>/', DecrementFromCartView.as_view(), name='decrement_from_cart'),
+    path('confirm-order/', ConfirmOrderView.as_view(), name='confirm_order'),
     path("accounts/", include("accounts_app.urls", namespace="accounts_app")),
-    path('my-orders/', MyOrdersView.as_view(), name='my_orders'),
     path("bookings/", include("bookings_app.urls", namespace="bookings_app"))
 ]
