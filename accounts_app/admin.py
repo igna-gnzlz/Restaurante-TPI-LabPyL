91af730c1c166f84bd966631c51a59920e478abf
eliezer-sardinas <sardinaseliezer@gmail.com>
2025-09-15 22:39:59 -0300

    agrego url para que recipients.html se renderize en el panel de admin

--- accounts_app/admin.py
+++ accounts_app/admin.py
@@ -5,6 +5,8 @@ from accounts_app.models import User, Notification, UserNotification
 from django.contrib.auth.models import Group
 from django.urls import reverse
 from django.utils.html import format_html
+from django.urls import path
+from django.shortcuts import get_object_or_404, render
 
 
 class UserAdmin(BaseUserAdmin):
@@ -31,10 +33,34 @@ class NotificationAdmin(admin.ModelAdmin):
     list_display = ('title', 'ver_destinatarios')  # Agregamos la columna
 
     def ver_destinatarios(self, obj):
-        url = reverse('accounts_app:notification_recipients', kwargs={'pk': obj.id})
+        url = reverse('admin:notification_recipients', kwargs={'pk': obj.id})
         return format_html('<a class="button" href="{}">Ver destinatarios</a>', url)
 
     ver_destinatarios.short_description = 'Destinatarios'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                '<int:pk>/recipients/',
+                self.admin_site.admin_view(self.recipients_view),
+                name='notification_recipients',
+            ),
+        ]
+        return custom_urls + urls
+
+    def recipients_view(self, request, pk):
+        notification = get_object_or_404(Notification, pk=pk)
+        user_notifications = UserNotification.objects.filter(notification=notification).select_related('user')
+
+        context = dict(
+            self.admin_site.each_context(request),
+            notification=notification,
+            user_notifications=user_notifications,
+            opts=self.model._meta,
+            title=f"Destinatarios de: {notification.title}",
+        )
+        return render(request, "admin/notification/recipients.html", context)
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
